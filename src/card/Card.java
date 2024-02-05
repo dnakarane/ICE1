d414f26e76c4803c1cd712333c4e7376c58551cc
DELL <DELL@10.48.9.233>
2024-02-05 15:15:28 -0500

    123

--- src/card/Card.java
+++ src/card/Card.java
@@ -1,24 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package card;
 
 /**
- * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
- * A suit (clubs, hearts, spades, diamonds).
+ * A class that models playing card objects. Cards have 
+ * a value (note that Ace = 1, Jack = 11, Queen = 12, King = 13)
+ * and a suit (hearts, diamonds, clubs, spades).
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author srinivsi
  */
 public class Card {
 
-   private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
+    private String suit; // clubs, spades, diamonds, hearts
+    private int value;   // 1-13
 
-   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+    public static final String[] SUITS = {"Hearts", "Diamonds", "Clubs", "Spades"};
+
+    // Constructor
+    public Card(int value, String suit) {
+        this.value = value;
+        this.suit = suit;
+    }
+
     /**
      * @return the suit
      */
@@ -46,7 +48,9 @@ public class Card {
     public void setValue(int value) {
         this.value = value;
     }
-   
-   
-    
+
+    @Override
+    public String toString() {
+        return suit + " " + value;
+    }
 }
--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -1,32 +1,60 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package card;
 
-/**
- * A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
- * @author srinivsi
- */
+import java.util.Random;
+import java.util.Scanner;
+
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        // Fill the magic hand with random cards
+        for (int i = 0; i < magicHand.length; i++) {
+            int value = random.nextInt(13) + 1; // Values from 1 to 13
+            String suit = Card.SUITS[random.nextInt(4)]; // Suits from the predefined SUITS array
+            magicHand[i] = new Card(value, suit);
         }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-        // add one luckcard hard code 2,clubs
+
+        // Print out the magic hand
+        for (Card card : magicHand) {
+            System.out.println(card);
+        }
+
+        // Ask the user for a card value
+        System.out.print("Enter a card value (1-13): ");
+        int userValue = scanner.nextInt();
+
+        // Ask the user for a suit using numbers
+        System.out.print("Enter a suit (0-3 where 0=Hearts, 1=Diamonds, 2=Clubs, 3=Spades): ");
+        int userSuitIndex = scanner.nextInt();
+        // Validate the suit index
+        if (userSuitIndex < 0 || userSuitIndex > 3) {
+            System.out.println("Invalid suit number entered. Please enter a number between 0 and 3.");
+            scanner.close();
+            return;
+        }
+        String userSuit = Card.SUITS[userSuitIndex];
+
+        Card userCard = new Card(userValue, userSuit);
+
+        // Search for the user's card in the magic hand
+        boolean found = false;
+        for (Card card : magicHand) {
+            if (card.getValue() == userValue && card.getSuit().equalsIgnoreCase(userSuit)) {
+                found = true;
+                break;
+            }
+        }
+
+        // Output the result
+        if (found) {
+            System.out.println("Your card is in the magic hand.");
+        } else {
+            System.out.println("Sorry, your card is not in the magic hand.");
+        }
+
+        scanner.close();
     }
-    
 }
